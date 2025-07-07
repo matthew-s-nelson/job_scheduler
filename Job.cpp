59470a941b3185e7179c6b1ad758a48952a086a0
Matthew Nelson <matthewbuzz@gmail.com>
2025-07-07 16:58:00 -0600

    Add basic implementation for Jobs

--- Job.cpp
+++ Job.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Job.h"
+
+Job::Job(std:function<void()> func,
+         Clock::time_point scheduledTime,
+         int priority)
+         : func_(func), scheduledTime_(scheduledTime), priority_(priority) {
+    uuid_t uuid;
+    uuid_generate(uuid);
+    char str[37];
+    uuid_unparse(uuid, str);
+    id_ = std::string(str);
+}
+
+void Job::execute() const {
+    func_();
+}
+
+std::string Job::getId() const {
+    return id_;
+}
+
+Job::Clock::time_point Job::getScheduledTime() const {
+    return scheduledTime_;
+}
+
+int Job::getPriority() const {
+    return priority_;
+}
--- Job.h
+++ Job.h
@@ -5,9 +5,31 @@
 #ifndef JOB_SCHEDULER_JOB_H
 #define JOB_SCHEDULER_JOB_H
 
+#pragma once
+#include <functional>
+#include <chrono>
+#include <string>
+#include <atomic>
+#include <uuid/uuid.h>
 
 class Job {
+public:
+    using Clock = std::chrono::steady_clock;
 
+    Job(std::function<void()> func,
+        Clock::time_point scheduledTime,
+        int priority = 0);
+
+    void execute() const;
+    std::string getId() const;
+    Clock::time_point getScheduledTime() const;
+    int getPriority() const;
+
+private:
+    std::function<void()> func_;
+    Clock::time_point scheduledTime_;
+    int priority_;
+    std::string id_;
 };
 
 
