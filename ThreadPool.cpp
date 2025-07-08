3906d9e0cdfa5040126bef4d550295abbc3ec31a
Matthew Nelson <matthewbuzz@gmail.com>
2025-07-08 07:35:28 -0600

    Add basic implementation for ThreadPool

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "ThreadPool.h"
+#include <iostream>
+#include <chrono>
+
+ThreadPool::ThreadPool(size_t numThreads): stop_(false) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers_.emplace_back(&ThreadPool::workerLoop, this);
+    }
+}
+
+void ThreadPool::enqueueJob(Job job) {
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        jobs_.push(job);
+    }
+    cv_.notify_one();
+}
+
+void ThreadPool::shutdown() {
+    stop_ = true;
+    cv_.notify_all();
+
+    for (std::thread worker : workers_) {
+        if (worker.joinable()) {
+            worker.join();
+        }
+    }
+}
+
+void ThreadPool::~ThreadPool() {
+    shutdown();
+}
+
+void ThreadPool::workerLoop() {
+    // Go until thread pool is shutdown
+    while (true) {
+        Job job([] {}, Job::Clock::now()); // dummy job
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+
+            cv_.wait(lock, [this]() {
+                return stop_ || !jobs_.empty();
+            });
+
+            // Only stop if all jobs are completed
+            if (stop_ && jobs_.empty()) return;
+
+            auto now = Job::Clock::now();
+
+            // Wait until we reach the next job's scheduled time
+            if (jobs_.top().getScheduledTime() > now) {
+                cv_.wait_until(lock, jobs_.top().getScheduledTime());
+                continue;
+            }
+
+            job = jobs_.top();
+            jobs_.pop();
+        }
+
+        std::cout << "[Thread " << std::this_thread::get_id() << "] Executing job: " << job.getId() << "\n";
+        job.execute();
+    }
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -5,9 +5,40 @@
 #ifndef JOB_SCHEDULER_THREADPOOL_H
 #define JOB_SCHEDULER_THREADPOOL_H
 
+#pragma once
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <functional>
+#include "Job.h"
 
 class ThreadPool {
+public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
 
+    // Add a job to the thread pool
+    void enqueueJob(std::function<void()> job);
+    // Shut down the thread pool and clean up resources
+    void shutdown();
+
+private:
+    void workerLoop();
+
+    struct JobComparator {
+        bool operator()(const Job& a, const Job& b) {
+            return a.getScheduledTime() > b.getScheduledTime();
+        }
+    };
+
+    std::priority_queue<Job, std::vector<Job>, JobComparator> jobs_;
+    std::vector<std::thread> workers_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::atomic<bool> stop_;
 };
 
 
